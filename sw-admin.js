eb6afa5e0ff3ae45ba3f2423d71ee0211e7eb98a
ais9839d <ais9839d@gmail.com>
2026-01-28 19:23:32 +0800

    Add files via upload

---
+++ sw-admin.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'admin-cache-v1';
+const ASSETS_TO_CACHE = [
+  './admin.html',
+  './logo.jpg',
+  './manifest-admin.json'
+];
+
+// 安裝並快取資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 啟用並清理舊快取
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 攔截請求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mknoni-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './logo.jpg'
+];
+
+// 安裝時快取基本資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 激活時清理舊快取
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 攔截請求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
